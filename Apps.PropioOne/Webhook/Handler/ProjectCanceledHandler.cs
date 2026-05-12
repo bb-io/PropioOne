c812e50898fbe17410c0d99c241856424c82b8fe
Artem Riabushenko <ariabushenko@blackbird.io>
2026-05-12 20:11:29 +0300

    Added new events

---
+++ Apps.PropioOne/Webhook/Handler/ProjectCanceledHandler.cs
@@ -0,0 +1,11 @@
+using Apps.PropioOne.Webhook.Model;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+
+namespace Apps.PropioOne.Webhook.Handler
+{
+    public class ProjectCanceledHandler(InvocationContext invocationContext, [WebhookParameter(true)] ProjectWebhookSettings setting) : BaseWebhookHandler(invocationContext, SubscriptionEvent, setting)
+    {
+        private const string SubscriptionEvent = "ProjectCanceled";
+    }
+}
